7952c51aa5bae32121bd6d066c1f282fbe2899e7
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-04-27 23:34:33 +0530

    Update find-first-and-last-position-of-element-in-sorted-array.java

--- java/binary-search/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ java/binary-search/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,15 +1,60 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
-        }
+    public int[] searchRange(int[] nums, int target) {
+      int[] arr = {-1,-1};
 
-        return num;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] > target){
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                arr[0] = lower(nums, target);
+                if (upper(nums, target) == -1){
+                    arr[1] = nums.length - 1;
+                } else {
+                    arr[1] = upper(nums, target) - 1;
+                }
+                return arr;
+            }
+        }
+        return arr;
+    }
+
+    private static int lower(int[] arr, int target){
+        int n = -1;
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (arr[mid] >= target){
+                n = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return n;
+    }
+
+    private static int upper(int[] arr, int target){
+        int n = -1;
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (arr[mid] > target){
+                n = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return n;
     }
 }
