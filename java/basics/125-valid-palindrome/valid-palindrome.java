bf0e43d884b7f3eb3463b09bfb6bb13f7f2dbd62
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-04-30 01:06:13 +0530

    Update valid-palindrome.java

--- java/basics/125-valid-palindrome/valid-palindrome.java
+++ java/basics/125-valid-palindrome/valid-palindrome.java
@@ -1,15 +1,33 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
-        }
+    public boolean isPalindrome(String s) {
+        s = s.trim();
+        s = s.toLowerCase();
+        String newStr = palindromeStr(s,"",1);
+        newStr = newStr.trim();
+        newStr = newStr.toLowerCase();
+        return newStr.equals(cleanStr(s,"",0));
+    }
 
-        return num;
+    private String cleanStr(String s, String str, int c){
+
+        if (c == s.length()){
+            return str;
+        }
+        if (Character.isLetterOrDigit(s.charAt(c))){
+            str = str + s.charAt(c);
+        }
+        c = c + 1;
+        return cleanStr(s,str,c);
+    }
+
+    private String palindromeStr(String s, String str, int c){
+        if (c > s.length()){
+            return str;
+        }
+        if (Character.isLetterOrDigit(s.charAt(s.length()-c))){
+            str = str + s.charAt(s.length()-c);
+        }
+        c = c + 1;
+        return palindromeStr(s, str, c);
     }
 }
