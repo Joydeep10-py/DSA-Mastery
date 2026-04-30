e1155d1ba3481d656a36f888b986b107696fec77
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-01 01:36:37 +0530

    Update remove-duplicates-from-sorted-array.java

--- java/arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ java/arrays/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -1,15 +1,12 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
+    public int removeDuplicates(int[] nums) {
+       HashSet<Integer> hash = new HashSet<>();
+        for(int i : nums){
+            hash.add(i);
         }
 
-        return num;
+        int k = hash.size();
+        List<Integer> list = new ArrayList<>(hash);
+        return k; 
     }
 }
