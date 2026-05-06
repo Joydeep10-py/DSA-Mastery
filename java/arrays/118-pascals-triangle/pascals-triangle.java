de2b8bc72fb7b91ce2bb01d0126efdefb2e0a652
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-06 22:12:10 +0530

    Update pascals-triangle.java

--- java/arrays/118-pascals-triangle/pascals-triangle.java
+++ java/arrays/118-pascals-triangle/pascals-triangle.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            ans.add(rowGenerate(i));
         }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
-        }
+        return ans;
 
-        return num;
+    }
+
+    private static List<Integer> rowGenerate(int n){
+        List<Integer> list = new ArrayList<>();
+        int ans = 1;
+        list.add(ans);
+        for (int i = 1; i < n; i++) {
+            ans = ans * (n - i);
+            ans =  ans / i;
+            list.add(ans);
+        }
+        return list;
     }
 }
