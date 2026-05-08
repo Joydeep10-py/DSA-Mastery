93e01ff228b23fa80745020fd2d8825cefcb6b31
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-08 16:12:18 +0530

    Update 3sum.java

--- java/arrays/15-3sum/3sum.java
+++ java/arrays/15-3sum/3sum.java
@@ -1,15 +1,40 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i >0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < 0){
+                    j++;
+                } else if (sum > 0) {
+                    k--;
+                } else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    j++;
+                    k--;
+                    ans.add(temp);
+                    while (j < k && nums[j] == nums[j-1]){
+                        j++;
+                    }
+                    while (j < k && nums[k] == nums[k+1]){
+                        k--;
+                    }
+                }
+            }
         }
 
-        return num;
+        return ans;
     }
 }
