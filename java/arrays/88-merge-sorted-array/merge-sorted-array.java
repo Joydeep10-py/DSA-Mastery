88682e3264d7aa2ca21559a8026b2a8d8e7f02e7
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-09 18:15:38 +0530

    Update merge-sorted-array.java

--- java/arrays/88-merge-sorted-array/merge-sorted-array.java
+++ java/arrays/88-merge-sorted-array/merge-sorted-array.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = nums1.length - 1;
+        if (m == 0){
+            for (int i = 0; i < nums2.length; i++) {
+                nums1[i] = nums2[i];
+            }
         }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
+        if (n == 0){
+            return;
         }
 
-        return num;
+        for (int i = 0; i < n; i++) {
+            nums1[l - i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
     }
 }
