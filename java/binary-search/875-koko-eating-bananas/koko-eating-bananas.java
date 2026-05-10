1dba0dd5e976dd304c408ec32fc6af0d86bdfbfb
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-10 16:07:55 +0530

    Update koko-eating-bananas.java

--- java/binary-search/875-koko-eating-bananas/koko-eating-bananas.java
+++ java/binary-search/875-koko-eating-bananas/koko-eating-bananas.java
@@ -1,15 +1,38 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
-        }
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = highestInArray(piles);
+        int ans = Integer.MAX_VALUE;
 
-        return num;
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            long hours = hoursTaken(piles,mid);
+            if (hours <= h){
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+
+    }
+
+    private static int highestInArray(int[] piles){
+        int max = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max){
+                max = piles[i];
+            }
+        }
+        return max;
+    }
+
+    private static long hoursTaken(int[] arr, int num){
+        long hours = 0;
+        for (int i = 0; i < arr.length; i++) {
+            hours += (int) Math.ceil((double) arr[i] /num);
+        }
+        return hours;
     }
 }
