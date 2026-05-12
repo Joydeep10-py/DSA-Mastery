c4e0db0727b0884b885b565f7076d3ff5e74c7f4
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-12 15:14:31 +0530

    Update find-the-smallest-divisor-given-a-threshold.java

--- java/binary-search/1283-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
+++ java/binary-search/1283-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -1,15 +1,36 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
-        }
+    public int smallestDivisor(int[] nums, int threshold) {
+        int low = 1;
+        int high = highestInArray(nums);
+        int ans = high;
 
-        return num;
+        while (low < high){
+            int mid = low + (high - low)/2;
+            int s = sumDiv(nums, mid);
+            if (s <= threshold){
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private static int sumDiv(int[] arr, int num){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += (int) Math.ceil((double) arr[i] / num);
+        }
+        return sum;
+    }
+
+    private static int highestInArray(int[] piles){
+        int max = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max){
+                max = piles[i];
+            }
+        }
+        return max;
     }
 }
