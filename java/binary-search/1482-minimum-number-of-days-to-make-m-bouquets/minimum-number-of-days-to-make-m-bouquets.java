7856ee64004c20f2dcb5713dca6a8e853dfcfa67
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-14 21:21:32 +0530

    Update minimum-number-of-days-to-make-m-bouquets.java

--- java/binary-search/1482-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ java/binary-search/1482-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,15 +1,47 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
+    public int minDays(int[] bloomDay, int m, int k) {
+        long num = (long) m * k;
+        if (num > bloomDay.length){
+            return -1;
         }
 
-        return num;
+        int low = 1;
+        int high = highestInArray(bloomDay);
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            int bou = bouquets(bloomDay, k, mid);
+            if (bou >= m){
+               high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+
+    private static int bouquets(int[] arr, int k, int num){
+        int c = 0;
+        int n = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] <= num){
+                c++;
+            } else {
+                n += c/k;
+                c = 0;
+            }
+        }
+        n += c/k;
+        return n;
+    }
+
+    private static int highestInArray(int[] piles){
+        int max = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max){
+                max = piles[i];
+            }
+        }
+        return max;
     }
 }
