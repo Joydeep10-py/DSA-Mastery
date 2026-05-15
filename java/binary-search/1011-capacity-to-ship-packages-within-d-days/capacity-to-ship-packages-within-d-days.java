d6c18fd5bda14d547ee9b1ac1ffc0163d72cf197
Joydeep Samanta <joydeepsamanta16@gmail.com>
2026-05-15 22:32:45 +0530

    Update capacity-to-ship-packages-within-d-days.java

--- java/binary-search/1011-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ java/binary-search/1011-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,15 +1,50 @@
 class Solution {
-    public int reverse(int x) {
-        int num = 0;
-        if (num>Integer.MAX_VALUE/10 || num<Integer.MIN_VALUE/10){
-            return 0;
-        }
-        while (x!=0){
-            int r = x % 10;
-            x = x / 10;
-            num = (num * 10) + r;
+    public int shipWithinDays(int[] weights, int days) {
+        int low = highestInArray(weights);
+        int high = sumationArray(weights);
+
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            int noDay = checkWeights(weights, mid);
+            if (noDay <= days){
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
         }
 
-        return num;
+        return low;
+    }
+
+    private static int sumationArray(int[] weights){
+        int sum = 0;
+        for(int i : weights){
+            sum += i;
+        }
+        return sum;
+    }
+
+    private static int checkWeights(int[] arr, int cap){
+        int day = 0;
+        int load = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (load + arr[i] > cap){
+                day += 1;
+                load = arr[i];
+            } else {
+                load += arr[i];
+            }
+        }
+        return day+1;
+    }
+
+    private static int highestInArray(int[] piles){
+        int max = 0;
+        for (int i = 0; i < piles.length; i++) {
+            if (piles[i] > max){
+                max = piles[i];
+            }
+        }
+        return max;
     }
 }
